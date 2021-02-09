4c82d652e64ce0a0046305fa75c64c9c49e6d5da
vagrant <vagrant@kmas1>
2021-02-09 06:49:07 +0000

    First example

---
+++ examples/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.15
+
+require github.com/lertrel/goforit v0.0.0-20210209062953-1ca723a76e64
---
+++ examples/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/lertrel/goforit"
+)
+
+func main() {
+
+	f := goforit.Get()
+
+str := `
+$SUMI(x, 2*x, 3*x, 4*x) + x
+`
+	c, err := f.LoadContext(nil, str)
+	if err != nil {
+		panic(err)
+	}
+
+	c.Set("x", 10.0)
+
+	jsVal, jsErr := c.Run(str)
+	if jsErr != nil {
+		panic(jsErr)
+	}
+
+	goVal, _ := jsVal.ToInteger()
+
+	fmt.Printf("goVal=%v\n", goVal)
+
+}
