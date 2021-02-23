8ec18187b36564bf8fbfdfa20589aa592f5eae8e
unknown <polymorphysm@gmail.com>
2021-02-23 14:56:56 +0700

    202102230259

--- formulas.go
+++ formulas.go
@@ -1,4 +1,12 @@
 package goforit
 
-type Formulas struct {
+//Formulas layer to help executing external formula related to
+//a pre-defined trigger point
+type Formulas interface {
+	Execute(trigger string, context map[string]string) (map[string]JSValue, error)
+}
+
+//FormulasBuilder a builder for Formulas
+type FormulasBuilder struct {
+	formularBuilder FormulaBuilder
 }
